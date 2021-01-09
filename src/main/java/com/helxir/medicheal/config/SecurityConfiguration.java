33874b424cbd36a8910470e59977e80a9417c72a
sanju-byte <74196076+sanju-byte@users.noreply.github.com>
2021-01-09 12:20:09 +0530

    Update SecurityConfiguration.java

--- src/main/java/com/helxir/medicheal/config/SecurityConfiguration.java
+++ src/main/java/com/helxir/medicheal/config/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		http.csrf().disable();
 		http.authorizeRequests()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 			.antMatchers("/user/**").hasRole("USER")
